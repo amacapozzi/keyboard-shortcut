6e24d6847b622c4ab006bf1aaebb41557c73e69f
amacapozzi <amacapozzi07@gmail.com>
2024-12-29 18:23:19 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module keymapper
+
+go 1.23.4
+
+require (
+	github.com/micmonay/keybd_event v1.1.2 // indirect
+	golang.design/x/hotkey v0.4.1 // indirect
+	golang.design/x/mainthread v0.3.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/micmonay/keybd_event"
+	"golang.design/x/hotkey"
+	"golang.design/x/hotkey/mainthread"
+)
+
+func main() {
+	mainthread.Init(fn)
+
+}
+func fn() {
+
+	for {
+
+		hk := hotkey.New([]hotkey.Modifier{hotkey.ModCtrl}, hotkey.KeyK)
+		err := hk.Register()
+		if err != nil {
+			log.Fatalf("hotkey: failed to register hotkey: %v", err)
+			return
+		}
+
+		msg, err := pressInsertKey()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(msg)
+
+		log.Printf("hotkey: %v is registered\n", hk)
+		<-hk.Keydown()
+		log.Printf("hotkey: %v is down\n", hk)
+		<-hk.Keyup()
+		log.Printf("hotkey: %v is up\n", hk)
+		hk.Unregister()
+		log.Printf("hotkey: %v is unregistered\n", hk)
+
+	}
+}
+
+func pressInsertKey() (string, error) {
+	kbf, err := keybd_event.NewKeyBonding()
+
+	if err != nil {
+		return "", err
+	}
+
+	kbf.SetKeys(keybd_event.VK_INSERT)
+	return "Insert pressed", nil
+}
